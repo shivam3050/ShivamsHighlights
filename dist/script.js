e9c47b46083b03d161445def050551953bce7ca1
shivam3050 <shivamkumar30508209@gmail.com>
2025-06-08 15:31:43 +0530

    updated

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,13 @@
 
-function downloadResumeFunction(event){ 
-
-        const resumeUrl = "https://raw.githubusercontent.com/shivam3050/MyResume/main/SHIVAM_CV_NITP_2027.pdf";
-        const a = document.createElement("a");
-        a.href = resumeUrl;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
+        document.getElementById("download-resume").addEventListener("click",()=>{
+                console.log("clicked")
+                const resumeUrl = "https://raw.githubusercontent.com/shivam3050/MyResume/main/SHIVAM_CV_NITP_2027.pdf";
+                const a = document.createElement("a");
+                a.href = resumeUrl;
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+        })
 
     
-}
+
